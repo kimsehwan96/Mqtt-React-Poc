58bfafb8738cba87ebebbb8b7ad8ac361553cbc9
Kimsehwan96 <sktpghks138@gmail.com>
2021-02-14 22:00:12 +0900

    add some example react code

--- frontend/mqtt-app/src/App.js
+++ frontend/mqtt-app/src/App.js
@@ -1,21 +1,22 @@
-import Amplify, { PubSub } from 'aws-amplify';
-import { AWSIoTProvider } from '@aws-amplify/pubsub/lib/Providers';
+import Amplify, {PubSub} from 'aws-amplify';
+import {AWSIoTProvider} from '@aws-amplify/pubsub/lib/Providers';
 import awsConfig from './awsConfig.json'
+import {useEffect, useState} from 'react';
 
-function init(awsConfig){
-      Amplify.configure({
-      Auth: {
-          userPoolId: awsConfig.cognitoUserPoolId,
-          userPoolWebClientId: awsConfig.cognitoUserPoolClientId,
-          identityPoolId: awsConfig.cognitoIdentityPoolId,
-          region: awsConfig.region,
-      }
-  });
+function init(awsConfig) {
+    Amplify.configure({
+        Auth: {
+            userPoolId: awsConfig.cognitoUserPoolId,
+            userPoolWebClientId: awsConfig.cognitoUserPoolClientId,
+            identityPoolId: awsConfig.cognitoIdentityPoolId,
+            region: awsConfig.region,
+        }
+    });
 
-  Amplify.addPluggable(new AWSIoTProvider({
-      aws_pubsub_region: awsConfig.region,
-      aws_pubsub_endpoint: `wss://${awsConfig.mqttBrokerEndpoint}/mqtt`,
-  }));
+    Amplify.addPluggable(new AWSIoTProvider({
+        aws_pubsub_region: awsConfig.region,
+        aws_pubsub_endpoint: `wss://${awsConfig.mqttBrokerEndpoint}/mqtt`,
+    }));
 }
 
 const topic = 'app/test' //우리가 임의로 지정 할 수 있는 mqtt subscribe 토픽
@@ -23,22 +24,42 @@ const topic = 'app/test' //우리가 임의로 지정 할 수 있는 mqtt subscr
 function App() {
     init(awsConfig);
 
-  PubSub.subscribe('app/test', {
-      provider: 'AWSIoTProvider'
-  }).subscribe({
-      next: (data) => console.log(data),
-      error: (error) => console.log(error)
-  })
+    const [fields, setFields] = useState([])
+    const [values, setValues] = useState([])
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>
-          Hello world
-        </h1>
-      </header>
-    </div>
-  );
+    useEffect(() => {
+        PubSub.subscribe('app/test', {
+            provider: 'AWSIoTProvider'
+        }).subscribe({
+            next: (data) => {
+                setFields(data.value.fields);
+                setValues(data.value.values);
+            },
+            error: (error) => console.log(error)
+        })
+    })
+
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                Hello App
+            </header>
+            <>
+                {
+                    fields.map((item, idx) => {
+                        console.log(values)
+                        return (
+                            <>
+                            <p> fields : {item} </p>
+                        <p> value : {values[0][idx]} </p>
+                            </>
+                        );
+                    })
+                }
+            </>
+        </div>
+    );
 }
 
 export default App;
